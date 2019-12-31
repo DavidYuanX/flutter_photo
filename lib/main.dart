b6ea357e3b05a27d6e1024ca20c965248ec19480
David <David.Yuan.X@gmail.com>
2020-01-01 00:03:32 +0800

    日夜间模式切换和字体切换。

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,46 @@ import 'package:flutter_photo/plugin_use.dart';
 import 'package:flutter_photo/res_page.dart';
 import 'package:flutter_photo/statefull_group_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(DynamicTheme());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class DynamicTheme extends StatefulWidget {
+  @override
+  _DynamicThemeState createState() => _DynamicThemeState();
+}
+
+class _DynamicThemeState extends State<DynamicTheme> {
+  Brightness _brightness = Brightness.light;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+//        fontFamily: 'RubikMonoOne',  /// 全局改变字体
+        brightness: _brightness,
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('如何使用路由导航'),
         ),
-        body: RouteNavigatorState(),
+        body: Column(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: (){
+                setState(() {
+                  if(_brightness == Brightness.dark){
+                    print(1);
+                    _brightness = Brightness.light;
+                  } else {
+                    _brightness = Brightness.dark;
+                  }
+                });
+              },
+              child: Text('切换主题abc',style: TextStyle(fontFamily: 'RubikMonoOne'),), /// 局部改变字体
+            ),
+            RouteNavigatorState()
+          ],
+        ),
       ),
       routes: <String,WidgetBuilder>{
         'Less':(BuildContext context)=>LessGroupPage(),
@@ -49,8 +64,8 @@ class MyApp extends StatelessWidget {
       },
     );
   }
-
 }
+
 // RouteNavigator
 class RouteNavigatorState extends StatefulWidget {
   @override
@@ -98,3 +113,44 @@ class _RouteNavigatorStateState extends State<RouteNavigatorState>{
   }
 
 }
+
+
+
+//class MyApp extends StatelessWidget {
+//  // This widget is the root of your application.
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Flutter Demo',
+//      theme: ThemeData(
+//        // This is the theme of your application.
+//        //
+//        // Try running your application with "flutter run". You'll see the
+//        // application has a blue toolbar. Then, without quitting the app, try
+//        // changing the primarySwatch below to Colors.green and then invoke
+//        // "hot reload" (press "r" in the console where you ran "flutter run",
+//        // or simply save your changes to "hot reload" in a Flutter IDE).
+//        // Notice that the counter didn't reset back to zero; the application
+//        // is not restarted.
+//        primarySwatch: Colors.blue,
+//      ),
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('如何使用路由导航'),
+//        ),
+//        body: RouteNavigatorState(),
+//      ),
+//      routes: <String,WidgetBuilder>{
+//        'Less':(BuildContext context)=>LessGroupPage(),
+//        'Plugin':(BuildContext context)=>PluginUse(),
+//        'Full':(BuildContext context)=>StateFullGroup(),
+//        'Gesture':(BuildContext context)=>GesturePage(),
+//        'ResPage':(BuildContext context)=>ResPage(),
+//        'LaunchPage':(BuildContext context)=>LaunchPage(),
+//        'WidgetLifecycle':(BuildContext context)=>WidgetLifecycle(),
+//        'AppLifecycle':(BuildContext context)=>AppLifecycle(),
+//
+//      },
+//    );
+//  }
+//}
